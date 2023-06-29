c0b7d4ed727500c39765c04f8ee3957f43c2c4f5
Mahmud Preyom <preyom5076@gmail.com>
2023-06-29 18:13:14 +0600

    4th commit

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,29 @@
 import './Footer.css'
+import {AiFillYoutube,AiFillGithub} from "react-icons/ai"
+import {BsFacebook} from "react-icons/bs"
 
 const Footer = () => {
     return (
-        <div>
-            
-        </div>
+        <footer>
+            <a href="#" className='footer__logo'>Mahmud Preyom</a>
+
+            <ul className='permalinks'>
+                <li><a href="#">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#experience">Experience</a></li>
+                <li><a href="#project">Project</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+
+            <div className="footer__socials">
+                <a href="https://web.facebook.com/mahmud.preyom"><BsFacebook/></a>
+                <a href="https://github.com/MahmudPreyom"><AiFillGithub/></a>
+                <a href="https://www.youtube.com/channel/UCjMxx9QlxCZWbHELuvZxU5A"><AiFillYoutube/></a>
+            </div>
+            <div className="footer__copyright">
+                <small>&copy; Mahmud Preyom. All Rights Reserved.</small>
+            </div>
+        </footer>
     );
 };
 
